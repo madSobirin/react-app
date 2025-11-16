91247431f42d62fca972bac08f3ddd69f2bc0d6b
madSobirin <gamingbrn202@gmail.com>
2025-11-16 16:05:40 +0700

    menambahkan auth layout pada auth

--- src/main.jsx
+++ src/main.jsx
@@ -7,26 +7,37 @@ import Login from "./pages/auth/Login.jsx";
 import Register from "./pages/auth/Register.jsx";
 import Book from "./pages/Book.jsx";
 import AuthLayout from "./layouts/AuthLayout.jsx";
+import NotFound from "./pages/404.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <AuthLayout>
+        <Login />
+      </AuthLayout>
+    ),
   },
   {
     path: "/register",
-    element: <Register />,
+    element: (
+      <AuthLayout>
+        <Register />
+      </AuthLayout>
+    ),
   },
   {
     path: "/books",
     element: <Book />,
   },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthLayout>
-      <RouterProvider router={router} />
-    </AuthLayout>
+    <RouterProvider router={router} />
   </StrictMode>
 );
